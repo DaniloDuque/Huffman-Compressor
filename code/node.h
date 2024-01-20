717236466f3dde8d61fe37d5fa30b9317e64e58f
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-01-19 20:17:34 -0600

    Create node.h

---
+++ code/node.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class node{
+
+    public:
+    
+        int frq;
+        unsigned char c;
+        node *left, *right;
+        node(int x, unsigned char y): frq(x), c(y) ,left(nullptr), right(nullptr){}
+        node(node *l, node *r) : c('\0'), left(l), right(r), frq(l->frq + r->frq){}
+        
+};
+

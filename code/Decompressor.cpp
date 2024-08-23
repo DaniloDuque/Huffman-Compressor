5fbd1ec24811befd47543711c2bd57e155cda584
Danilo <105374584+DaniloDuque@users.noreply.github.com>
2024-08-22 23:29:40 -0600

    Update Decompressor.cpp

--- code/Decompressor.cpp
+++ code/Decompressor.cpp
@@ -1,15 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 unordered_map<string, unsigned char> huffCodes;
 
-
-
-
-
 string decompress(string zip){
-
     string rslt = "", curr = "";
     for(unsigned char byte: zip)
         for(int i = 7; i>=0; i--){                        //read each bit, of the string zip
@@ -18,56 +12,31 @@ string decompress(string zip){
                 rslt += huffCodes[curr];
                 curr = "";
             }
-            
-
         }
-
     return rslt;
-    
 }
 
-
-
-
-
-
-
-
 void readTable(string &name){
-
     ifstream table(name);
     int ch; string route;
     while(table >> ch >> route) huffCodes[route] = ch;
     table.close();
-
 }
 
-
 string readInput(string &name){
- 
+
     ifstream in(name, ios::binary);
     string data((istreambuf_iterator<char>(in)), (istreambuf_iterator<char>()));
     in.close();
     return data;
-
 }
 
-
 void writeInOut(string &name, string data){
-
     ofstream out(name, ios::binary);
     out.write(data.data(), data.size());
     cout<<"Successful decompression on: "<<name<<'\n'; 
     out.close();
-
 }
-
-
-
-
-
-
-
 
 int main(){
 
